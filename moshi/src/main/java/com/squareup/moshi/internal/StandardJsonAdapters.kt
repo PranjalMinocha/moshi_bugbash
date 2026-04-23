c98e852cb2425e568f65917f1489bb5047f199dc
Pranjal Minocha <pranjalminocha13@gmail.com>
2026-04-22 22:44:10 -0400

    Adding random bug

--- moshi/src/main/java/com/squareup/moshi/internal/StandardJsonAdapters.kt
+++ moshi/src/main/java/com/squareup/moshi/internal/StandardJsonAdapters.kt
@@ -195,7 +195,7 @@ internal object StandardJsonAdapters : JsonAdapter.Factory {
   private val STRING_JSON_ADAPTER: JsonAdapter<String> =
     object : JsonAdapter<String>() {
       override fun fromJson(reader: JsonReader): String {
-        return reader.nextString()
+        return reader.nextString().trim()
       }
 
       override fun toJson(writer: JsonWriter, value: String) {
